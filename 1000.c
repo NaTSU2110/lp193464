144073792d326146cbab235fa206268a052f5092
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-04-19 15:26:57 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -2,7 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Natália Tenório Silva Uemura
 Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
 Data        : 08/04/2026
 Objetivo    : Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha
 Aprendizado : Adicionar biblioteca e utilizar a função printf
