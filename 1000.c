2334ac31a7d893e0c529b5737042508ff432c51b
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-04-19 15:25:49 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 08/04/2026
+Objetivo    : Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha
+Aprendizado : Adicionar biblioteca e utilizar a função printf
+*/
+    
 #include <stdio.h>
 
 int main(){
