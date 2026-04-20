517a872ed12d8ce91d9a8b4d6f1ad23074321262
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-04-20 17:16:42 -0300

    Update 1080a.c

--- 1080a.c
+++ 1080a.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Natália Tenório Silva Uemura
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1080
-Data        : 08/04/2026
-Objetivo    : Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha
-Aprendizado : Adicionar biblioteca e utilizar a função printf
+Data        : 20/04/2026
+Objetivo    : Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Aprendizado : utilizar for para declarar variáveis
 */
