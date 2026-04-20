bca96da04ad10ba49c0758c3d324c60cd637d088
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-04-20 17:15:30 -0300

    Create 1080b.c

---
+++ 1080b.c
@@ -0,0 +1,9 @@
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 20/04/2026
+Objetivo    : Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Aprendizado : utilizar vetor
+*/
