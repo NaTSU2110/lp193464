d4c35b7b4b15710ed0a62d2155df28ccef2fe6e4
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-04-27 18:42:26 -0300

    Update 1234.c

--- 1234.c
+++ 1234.c
@@ -1,3 +1,15 @@
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : 27/04/2026
+Objetivo    : Transforme a sentença de entrada em uma sentença 
+dançante trocando as letras para minúscula ou maiúscula. Todos
+os espaços da sentença original deverão ser preservados.
+Aprendizado : utilizar melhor a função if, a tabela ascii, 
+utilizar string, e conversão para maiúsculas e minúsculas
+*/
 #include <stdio.h>
 #include <ctype.h>
 
