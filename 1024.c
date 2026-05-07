03ac5746267cb79c741f214514bdf3f78d73ea46
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-05-07 14:28:01 -0300

    Create 1024.c

---
+++ 1024.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int N, n;
+    char finald, crip[1001];
+
+    scanf("%d\n", &N);
+
+    for(int i = 0; i < N; ++i){
+        scanf("%[^\n]\n", &crip);
+        n = strlen(crip);
+        
+        for(int j = 0; j < n; ++j){
+            if (65<=crip[j] && crip[j]<=90 || 97<=crip[j] && crip[j]<=122){
+                crip[j] += 3;
+            }
+        }
+        for(int j = 0; j < n/2; ++j){
+            finald = crip[j];
+            crip[j] = crip[n - 1 - j];
+            crip[n - 1 - j] = finald;
+        }
+        for(int j = n/2; j < n; ++j){
+            crip[j]-=1;
+        }
+        printf("%s\n", crip);
+    }
+
+    return 0;
+}
