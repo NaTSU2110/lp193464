46a71b09afc576ec1b41800a4fc7acbf7209fd54
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-05-21 15:09:19 -0300

    Update 1534a.c

--- 1534a.c
+++ 1534a.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main(){
+    int i,j,k,n,matriz[72][72];
+    while(scanf("%d",&n)!=EOF){
+        k=n-1;;
+        for(i = 0;i < n;i++){
+            for(j = 0;j < n;j++){
+                if(i==j)matriz[i][j]=1;
+                else matriz[i][j]=3;
+                if(j==k)matriz[i][j]=2;
+            }
+            k--;
+        }
+        for(i = 0;i < n;i++){
+            for(j = 0;j < n;j++){
+                printf("%d",matriz[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
