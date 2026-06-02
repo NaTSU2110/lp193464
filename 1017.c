a6b8659689f38fb0a7788ccb3ca300308f899bd3
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-01 21:17:50 -0300

    Update 1017.c

--- 1017.c
+++ 1017.c
@@ -1,3 +1,12 @@
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 19/05/2026
+Objetivo    : calcular litros de combustível a partir de dada velocidade e tempo
+Aprendizado : usar função
+*/
 
 #include <stdio.h>
 
