f009cb508946b8b5a3061a288f837ae4da36f969
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-01 22:06:00 -0300

    Update 1024.c

--- 1024.c
+++ 1024.c
@@ -1,3 +1,12 @@
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 04/05/2026
+Objetivo    : mensagem codificada
+Aprendizado : string
+*/
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
