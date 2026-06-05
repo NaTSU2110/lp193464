2082e5558ef7bfcd12f6aef3bfe578738acf4ec4
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-05 13:27:18 -0300

    Update 1258.c

--- 1258.c
+++ 1258.c
@@ -1,4 +1,12 @@
-
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1258
+Data        : 25/05/2026
+Objetivo    : lista para produção de camisetas
+Aprendizado : struct
+*/
 #include <stdio.h>
 #include <string.h>
 
