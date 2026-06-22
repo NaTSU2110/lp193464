5af03bfc139434e96ed16f0e19bc03c3a984054a
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-22 19:45:14 -0300

    Update 1031.cc

--- 1031.cc
+++ 1031.cc
@@ -1,3 +1,12 @@
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1031
+Data        : 22/06/2026
+Objetivo    : determinar menor m, conservando o 13 como última região
+Aprendizado : c++.
+-------------------------------------------------------------------------- */
 #include <iostream>
 
 int corte(int N, int m) {
