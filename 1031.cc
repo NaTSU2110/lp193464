bd1a23eb53c3f434e94b85f29e7a0eb81f344ad6
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-22 19:36:55 -0300

    Update 1031.cc

--- 1031.cc
+++ 1031.cc
@@ -2,26 +2,20 @@
 
 int corte(int N, int m) {
     int ultima = 0;
-
     for (int i = 1; i < N; i++) {
         ultima = (ultima + m) % i;
     }
-
     return ultima + 1;
 }
 
 int main() {
     int N;
-
     while (std::cin >> N && N != 0) {
         int m = 1;
-
         while (corte(N, m) != 12) {
             m++;
         }
-
         std::cout << m << '\n';
     }
-
     return 0;
 }
