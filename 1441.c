12798ef030f95b8c0808a6b6bd38dbd6012264f5
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-22 12:31:03 -0300

    Update 1441.c

--- 1441.c
+++ 1441.c
@@ -1,1 +1,10 @@
-a
+/*
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Natália Tenório Silva Uemura
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1441
+Data        : 22/06/2026
+Objetivo    : Sequências de granizo
+Aprendizado : 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
