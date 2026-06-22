a4c0ee41c285bd2d4b33866f0bacad759203c3b5
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-22 13:38:16 -0300

    Update 1441.c

--- 1441.c
+++ 1441.c
@@ -4,7 +4,34 @@ Nome        : Natália Tenório Silva Uemura
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1441
 Data        : 22/06/2026
-Objetivo    : Sequências de granizo
-Aprendizado : 
+Objetivo    : Achar o maior número das sequências de granizo
+Aprendizado : trabalhar com recursão.
 -------------------------------------------------------------------------- */
 #include <stdio.h>
+
+int maior = 1, n;
+int granizo(int H){
+    if (H==1){
+        return maior;
+    }
+    if (H>maior){
+        maior = H;
+    }
+    if (H%2==0){
+        n = H/2;
+    }
+    else{
+        n = 3*H + 1;
+    }
+    return granizo(n);
+}
+
+int main(){
+    int H;
+    while(scanf("%d", &H) && H != 0){
+        maior = 1;
+        maior = granizo(H);
+        printf("%d\n", maior);
+    }
+    return 0;
+}
