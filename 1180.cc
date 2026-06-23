5b743670e78260619b2b215eb953e4730b126833
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-23 09:55:59 -0300

    Create 1180.cc

---
+++ 1180.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> X(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> X[i];
+    }
+
+    int menor = X[0];
+    int pos = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (X[i] < menor) {
+            menor = X[i];
+            pos = i;
+        }
+    }
+
+    cout << "Menor valor: " << menor << '\n';
+    cout << "Posicao: " << pos << '\n';
+
+    return 0;
+}
