d2955fedfde64ccd92e9d0d629b3444c4d14d2ee
NaTSU2110 <uemura.tenorio@unifesp.br>
2026-06-23 16:16:15 -0300

    Update 1234.c

--- 1234.c
+++ 1234.c
@@ -13,25 +13,21 @@ utilizar string, e conversão para maiúsculas e minúsculas
 #include <stdio.h>
 #include <ctype.h>
 
-int main(){
+int main() {
     char dance[50];
-    int i, letra=1;
-    fgets (dance,50,stdin);
-        for (i=0;i<50;i++){
-            if (65<=dance[i] && dance[i]<=90 || 97<=dance[i] && dance[i]<=122){
-                if (letra%2!=0){
-                dance[i]=toupper(dance[i]);
+    while (fgets(dance, sizeof(dance), stdin)) {
+        int i, letra = 1;
+        for (i = 0; dance[i] != '\0'; i++) {
+            if (isalpha(dance[i])) {
+                if (letra % 2 != 0)
+                    dance[i] = toupper(dance[i]);
+                else
+                    dance[i] = tolower(dance[i]);
+
                 letra++;
-                }
-                else{
-                dance[i]=tolower(dance[i]);
-                letra++;
-                }
-            }
-            else{
-                dance[i]=dance[i];
             }
         }
-    printf("%s\n",dance);
+        printf("%s", dance);
+    }
     return 0;
 }
